6fb1c948ef273dd63b571f40bd5377e234cbbee6
Naveen Gaur <naveensgour@gmail.com>
2026-09-16 20:33:08 +0530

    loading added on link click

---
+++ src/app/exam/loading.tsx
@@ -0,0 +1,5 @@
+import { RouteLoading } from '@/components/exam/RouteLoading';
+
+export default function ExamLoading() {
+  return <RouteLoading label="Loading the paper…" />;
+}
---
+++ src/app/spreadsheet/loading.tsx
@@ -0,0 +1,5 @@
+import { RouteLoading } from '@/components/exam/RouteLoading';
+
+export default function SpreadsheetLoading() {
+  return <RouteLoading label="Opening the spreadsheet…" />;
+}
